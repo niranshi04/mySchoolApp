887a0a4b909698cc9c7626851ba2485e87c1c2ad
niranshi04 <niranshi.agarwal.cse19@itbhu.ac.in>
2021-11-29 06:14:42 +0530

    Changing url for email

--- mySchoolApp/mySchoolApp/src/main/java/com/dbms/mySchoolApp/utils/ServerUtil.java
+++ mySchoolApp/mySchoolApp/src/main/java/com/dbms/mySchoolApp/utils/ServerUtil.java
@@ -7,7 +7,7 @@ public class ServerUtil {
     public String getHostAddressAndPort() {
         String hostname = System.getenv("SERVER_HOSTNAME");
         if (hostname == null) {
-            return "http://127.0.0.1:8080";
+            return "https://my-school-application.herokuapp.com";
         } else {
             return hostname;
         }
